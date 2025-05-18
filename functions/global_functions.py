ed388ad65d33fe44e6a8674753e626eb6f6a9d53
Einarby <123009379+Einarby@users.noreply.github.com>
2025-05-18 15:07:39 +0200

    isin fix i global functions og endringer i benchmark db

--- functions/global_functions.py
+++ functions/global_functions.py
@@ -1668,7 +1668,7 @@ def GetReservoirFillingAtSpecificNodes(Nodes, data: GridData, database: Database
     storage_data = data.generator[
         (data.generator["node"].isin(Nodes)) &
         (data.generator["storage_cap"] > 0) &
-        (data.generator["type"] in ["hydro", "ror"])
+        (data.generator["type"].isin(["hydro", "ror"]))
     ][["node", "storage_cap"]]
 
     storage_idx = storage_data.groupby("node").apply(lambda x: list(x.index)).to_dict()
